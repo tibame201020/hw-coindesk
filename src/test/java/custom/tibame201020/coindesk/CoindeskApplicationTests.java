e148feadcf409440bf90ec3b63f74af96ef899d6
USER\jeffhsu <tibame201020@gmail.com>
2024-08-11 21:35:38 +0800

    init

---
+++ src/test/java/custom/tibame201020/coindesk/CoindeskApplicationTests.java
@@ -0,0 +1,13 @@
+package custom.tibame201020.coindesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoindeskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
