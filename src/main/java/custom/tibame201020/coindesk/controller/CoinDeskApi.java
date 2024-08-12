0e35c1844db0b4ba3a207dc7c6508b3fc35d8cfe
USER\jeffhsu <tibame201020@gmail.com>
2024-08-12 09:59:05 +0800

    add spring doc

--- src/main/java/custom/tibame201020/coindesk/controller/CoinDeskApi.java
+++ src/main/java/custom/tibame201020/coindesk/controller/CoinDeskApi.java
@@ -1,10 +1,10 @@
 package custom.tibame201020.coindesk.controller;
 
 import custom.tibame201020.coindesk.client.BitCoinClient;
-import custom.tibame201020.coindesk.domain.Currency;
 import custom.tibame201020.coindesk.dto.BitCoinRateData;
 import custom.tibame201020.coindesk.dto.CurrencyDTO;
 import custom.tibame201020.coindesk.service.CurrencyService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -26,6 +26,7 @@ public class CoinDeskApi {
      * get bit-coin rate origin data
      * @return BitCoinRateData
      */
+    @Operation(summary = "coindesk API,並顯示其內容", description = "Fetches the current Bitcoin rate data directly from the CoinDesk API.")
     @GetMapping("/origin")
     public BitCoinRateData getOriginCurrentPriceRate() {
         return bitCoinClient.getCurrentPriceRate();
@@ -33,8 +34,9 @@ public class CoinDeskApi {
 
     /**
      * get converted bit-coin rate data
-     * @return
+     * @return list currencyDTO
      */
+    @Operation(summary = "資料轉換的 API,並顯示其內容", description = "Converts the Bitcoin rate data from the CoinDesk API to a format that includes currency descriptions and rates in the database.")
     @GetMapping("/convert")
     public List<CurrencyDTO> getCovertCurrentPriceRate() {
         BitCoinRateData bitCoinRateData = bitCoinClient.getCurrentPriceRate();
--- src/main/java/custom/tibame201020/coindesk/controller/CurrencyApi.java
+++ src/main/java/custom/tibame201020/coindesk/controller/CurrencyApi.java
@@ -3,6 +3,7 @@ package custom.tibame201020.coindesk.controller;
 import custom.tibame201020.coindesk.domain.Currency;
 import custom.tibame201020.coindesk.dto.CurrencyDTO;
 import custom.tibame201020.coindesk.service.CurrencyService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
@@ -20,6 +21,7 @@ public class CurrencyApi {
      * get all currencies
      * @return all currencies
      */
+    @Operation(summary = "Get all currencies", description = "Fetches a list of all available currencies from the database.")
     @GetMapping("/all")
     public List<Currency> getAllCurrencies() {
         return currencyService.getAllCurrencies();
@@ -30,6 +32,7 @@ public class CurrencyApi {
      * @param coinCategory 幣別
      * @return currency data
      */
+    @Operation(summary = "查詢幣別對應表資料 API,並顯示其內容", description = "Fetches the details of a specific currency based on its category (coinCategory).")
     @GetMapping("/{coinCategory}")
     public Currency getCurrency(@PathVariable("coinCategory") String coinCategory) {
         return currencyService.getCurrency(coinCategory);
@@ -40,6 +43,7 @@ public class CurrencyApi {
      * @param currencyDTO currencyDTO
      * @return created currency data
      */
+    @Operation(summary = "新增幣別對應表資料 API", description = "Creates a new currency entry in the database using the provided details in currencyDTO.")
     @PostMapping
     public Currency createCurrency(@RequestBody CurrencyDTO currencyDTO) {
         return currencyService.createCurrency(
@@ -54,6 +58,7 @@ public class CurrencyApi {
      * @param currencyDTO currencyDTO
      * @return updated currency data
      */
+    @Operation(summary = "更新幣別對應表資料 API,並顯示其內容", description = "Updates the details of an existing currency based on its category (coinCategory).")
     @PatchMapping("/{coinCategory}")
     public Currency updateCurrency(@PathVariable("coinCategory") String coinCategory, @RequestBody CurrencyDTO currencyDTO) {
         return currencyService.updateCurrency(
@@ -67,6 +72,7 @@ public class CurrencyApi {
      * delete currency by coinCategory
      * @param coinCategory 幣別
      */
+    @Operation(summary = "刪除幣別對應表資料 API", description = "Deletes a currency from the database based on its category (coinCategory).")
     @DeleteMapping("/{coinCategory}")
     public void deleteCurrency(@PathVariable("coinCategory") String coinCategory) {
         currencyService.deleteCurrency(coinCategory);
